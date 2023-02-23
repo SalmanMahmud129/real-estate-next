6b38dd834009099e2639d1d76bcd76a4de2eb0be
Salman Mahmud <salman_mahmud@hotmail.com>
2023-02-23 16:42:06 -0500

    added to property format hook to account for single property page

--- features/common/Hooks/usePropertyFormat.js
+++ features/common/Hooks/usePropertyFormat.js
@@ -29,6 +29,32 @@ const sqSize = property.area.toFixed(2)
 
 const externalID = property.externalID
 
+
+// pulling single property data
+
+// if a photo exists in the photo array, map through and give the url for each, otherwise 
+// give an empty array
+const photos = property.photos?.map((photo) => photo.url) || []
+
+const description = property.description
+
+// link in json is the share link and not the embed link so we need to slice
+// the id off the share link and use it to create an embed link
+const coverVideoUrl = property.coverVideo.url
+// start at the end and strip off 11 which gives us the id
+const coverVideo = coverVideoUrl.slice(coverVideoUrl.length - 11)
+
+// if panoramas array contains something then give us the first object in the array
+// otherwise give an empty array
+const panorama = property.panoramas?.length ? property.panorama[0].url : []
+
+// flatmap condenses all nested arrays into one. Then extract/map from the second amenities array 
+const amenities = property.amenities?.flatMap(({amenities}) => amenities).map((item) => item.text)
+
+const furnished = property.furnishingStatus
+
+
+
   return {
     address,
     coverPhoto,
@@ -39,7 +65,13 @@ const externalID = property.externalID
     baths,
     purpose,
     sqSize,
-    externalID
+    externalID,
+    photos,
+    description,
+    coverVideo,
+    panorama,
+    amenities,
+    furnished
   }
 }
 
